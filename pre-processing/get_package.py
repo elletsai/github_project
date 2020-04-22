10c90e5d1505f308fcc75ee660ea6c6819546a3f
elletsai <46097002+elletsai@users.noreply.github.com>
2020-04-22 13:25:22 -0400

    Update get_package.py

--- pre-processing/get_package.py
+++ pre-processing/get_package.py
@@ -1,4 +1,4 @@
-def get_py_module(df, save_name):
+def get_py_module(df, save_path):
 
     import pandas as pd
     import dis
@@ -64,7 +64,7 @@ def get_py_module(df, save_name):
             df_python['module'][i] = list(chain.from_iterable(import_name))
 
 
-        df_python.to_csv('/sfs/lustre/bahamut/scratch/yt9mh/RA-github/github/'+str(save_name)+'.csv', index = False)
+        df_python.to_csv(save_path, index = False)
 
     return df_python
 
